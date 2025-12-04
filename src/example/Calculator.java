c49e92adbd084160c8c1319a557cc57a7b3d283a
youzting <158013348+youzting@users.noreply.github.com>
2025-12-04 12:37:20 +0900

    양의 정수 입력 코드 및 사칙 연산 수행 코드 구현

---
+++ src/example/Calculator.java
@@ -0,0 +1,46 @@
+package example;
+
+
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c1, c2 = 0;
+        while(true){
+            System.out.println("첫 번째 숫자를 입력하세요.");
+            c1 = scanner.nextInt();
+            if (c1 >= 0){
+                break;
+            }
+            System.out.println("양의 정수만 입력하세요.");
+
+        }
+        while(true){
+            System.out.println("두 번째 숫자를 입력하세요.");
+            c2 = scanner.nextInt();
+            if (c2 >= 0){
+                break;
+            }
+            System.out.println("양의 정수만 입력하세요.");
+
+        }
+        System.out.println("사칙연산을 입력하세요");
+        String ao =  scanner.next();
+        if (ao.equals("+")){
+            System.out.println("연산결과:" +(c1 + c2));
+        }
+        else if (ao.equals("-")){
+            System.out.println("연산결과:" +(c1 - c2));
+        }
+        else if (ao.equals("*") || ao.equals("x") || ao.equals("X")){
+            System.out.println("연산결과:" +(c1 * c2));
+        }
+        else if (ao.equals("/")){
+            System.out.println("연산결과:" +(c1 / c2));
+        }
+    }
+
+
+}
